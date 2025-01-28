1d2376c80d960d1c21586f362219c1a82ba66540
kishan-singh <kishan.singh@rea-group.com>
2025-01-28 14:14:09 +0530

    Initial Commit

---
+++ CatsVsDogs/CatsVsDogsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsVsDogsApp.swift
+//  CatsVsDogs
+//
+//  Created by Kishan Singh on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CatsVsDogsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatsVsDogsTests/CatsVsDogsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatsVsDogsTests.swift
+//  CatsVsDogsTests
+//
+//  Created by Kishan Singh on 28/01/25.
+//
+
+import Testing
+@testable import CatsVsDogs
+
+struct CatsVsDogsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
