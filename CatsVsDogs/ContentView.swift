fb504056ad36fe10a0531d7041ae8fbf1e239c2f
kishan-singh <kishan.singh@rea-group.com>
2025-02-16 23:50:11 +0530

    created CoreML project in iOS

--- CatsVsDogs/ContentView.swift
+++ CatsVsDogs/ContentView.swift
@@ -1,24 +1,110 @@
 //
 //  ContentView.swift
-//  CatsVsDogs
+//  HelloCoreML
 //
-//  Created by Kishan Singh on 28/01/25.
+//  Created by Mohammad Azam on 5/19/23.
 //
 
 import SwiftUI
+import CoreML
+import PhotosUI
+
+struct ProbabilityListView: View {
+    
+    let probs: [Dictionary<String, Double>.Element]
+    
+    var body: some View {
+        List(probs, id: \.key) { (key, value) in
+            
+            HStack {
+                Text(key)
+                Text(NSNumber(value: value), formatter: NumberFormatter.percentage)
+            }
+        }
+    }
+}
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    let model = try! CatsVsDogsImageClassifier(configuration: MLModelConfiguration())
+    @State private var probs: [String: Double] = [: ]
+    @State private var selectedPhotoItem: PhotosPickerItem? = nil
+    @State private var uiImage: UIImage? = UIImage(named: "cat_113")
+    @State private var isCameraSelected: Bool = false
+    
+    var sortedProbs: [Dictionary<String, Double>.Element] {
+        
+        let probsArray = Array(probs)
+        return probsArray.sorted { lhs, rhs in
+            lhs.value > rhs.value
         }
+    }
+    
+    var body: some View {
+        
+        VStack {
+            
+            if let uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300, height: 300)
+            }
+            
+            HStack {
+                
+                PhotosPicker(selection: $selectedPhotoItem, matching: .images) {
+                    Text("Select a Photo")
+                }
+                
+                Button("Camera") {
+                    isCameraSelected = true
+                }.buttonStyle(.bordered)
+                    
+            }
+            
+            
+            Button("Predict") {
+                
+                guard let resizedImage = uiImage?.resizeTo(to: CGSize(width: 299, height: 299)) else {
+                    return
+                }
+                guard let buffer = resizedImage.toCVPixelBuffer() else { return }
+                
+                do {
+                    let result = try model.prediction(image: buffer)
+                    probs = result.classLabelProbs
+                } catch {
+                    print(error.localizedDescription)
+                }
+                
+            }.buttonStyle(.borderedProminent)
+            
+            ProbabilityListView(probs: sortedProbs)
+        }
+      
+        .onChange(of: selectedPhotoItem, perform: { selectedPhotoItem in
+            selectedPhotoItem?.loadTransferable(type: Data.self, completionHandler: { result in
+                switch result {
+                    case .success(let data):
+                        if let data {
+                            guard let img = UIImage(data: data) else { return }
+                            uiImage = img
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                }
+            })
+        })
+        .sheet(isPresented: $isCameraSelected, content: {
+            ImagePicker(image: $uiImage, sourceType: .camera)
+        })
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
